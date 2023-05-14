53fa0f1ff8a0e8debc36f09567950dd11fc09db3
gittjar <jarno.keranen@gmail.com>
2023-05-14 10:59:54 +0300

    changing get data portfolio patch

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,4 +12,10 @@ export class PortfolioComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  angulars = [
+    {"id": 1001, "name": "Keep Clean", "update": 17-4-2023, "detail": "Helsinki Business Collegen Demo Session kevät 2023 tehty webapp, joka on informaatiopeneeli yleisten wc -tilojen käyttäjille. Ohjelma antaa informaatiota viimeisestä siivouskerrasta. Käyttömuoto: Tabletti vaakatasossa.",
+  "status":"ready"},
+
+  ]
+
 }
