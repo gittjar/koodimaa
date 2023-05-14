0443911cca0e2941f6d4bc34d8da28234c244893
gittjar <jarno.keranen@gmail.com>
2023-05-14 20:00:53 +0300

    csharp part updated in portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -22,9 +22,14 @@ export class PortfolioComponent implements OnInit {
   ]
 
   csharp = [
-    {"id": 2001, "name": "Movie MVC","url":"https://jarnonmovieapp.azurewebsites.net/Movies", "update": "päivitetty: 26.4.2023", "detail": "Ohjelma tallentaa Azure SQL palvelimelle annetut tiedot (CRUD). Azure SQL palvelimeen otetaan yhteys rajapinnalla ja rajapintaa voi lukea esim Asp.NET, Angularilla tai vastaavalla frontendillä.","status":"valmis"},
-    {"id": 2002, "name": "Moviestore","url":"https://jarnomovies.azurewebsites.net/", "update": "päivitetty: 15.4.2023", "detail": "Ohjelma lukee ylläolevan MVC Movien rajapinnasta dataa ja antaa ns. käyttäjänäkymän erikseen määritetyistä kentistä. Voit testata toimintaa lisäämällä/poistamalla MVC Movies -ohjelmaan dataa.","status":"valmis"},
-    {"id": 2003, "name": "Get Movie Tickets", "url":"https://github.com/gittjar/GetMovieTickets", "update":"päivitetty 1.5.2023", "detail": "Konsoliohjelma johon käyttäjä voi lisätä käyttäjiä ja poistaa. Lisäksi 3 eri elokuvaa joihin mahdollisuus ostaa paikkoja elokuvateatterista. Ohjelma tallentaa tiedot SQLitellä tietokantaan.", "status":"valmis"},
+    {"id": 2001, "name": "Movie MVC","url":"https://jarnonmovieapp.azurewebsites.net/Movies", "update": "päivitetty: 26.4.2023", "detail": "Ohjelma tallentaa Azure SQL palvelimelle annetut tiedot (CRUD). Azure SQL palvelimeen otetaan yhteys rajapinnalla ja rajapintaa voi lukea esim Asp.NET, Angularilla tai vastaavalla frontendillä. Julkaistu käyttäen Azure pipelines","status":"valmis"},
+    {"id": 2002, "name": "Moviestore","url":"https://jarnomovies.azurewebsites.net/", "update": "päivitetty: 15.4.2023", "detail": "Ohjelma lukee ylläolevan MVC Movien rajapinnasta dataa ja antaa ns. käyttäjänäkymän erikseen määritetyistä kentistä. Voit testata toimintaa lisäämällä/poistamalla MVC Movies -ohjelmaan dataa. Julkaistu käyttäen Azure pipelines.","status":"valmis"},
+    {"id": 2003, "name": "Get Movie Tickets", "url":"https://github.com/gittjar/GetMovieTickets", "update":"päivitetty 1.5.2023", "detail": "Konsoliohjelma C# johon käyttäjä voi lisätä käyttäjiä ja poistaa. Lisäksi 3 eri elokuvaa joihin mahdollisuus ostaa paikkoja elokuvateatterista. Ohjelma tallentaa tiedot SQLitellä tietokantaan.", "status":"valmis"},
+    {"id": 2004, "name": "Rent Index Calculator", "url":"https://github.com/gittjar/RentIndexCheck", "update":"päivitetty 6.2.2023", "detail": "Konsoliohjelma C#, tarkista indeksikortus vuokraasi helposti tällä ohjelmalla. Kielipaketti ENG tekeillä.", "status":"kesken"},
+    {"id": 2005, "name": "BiisiGeneraattori", "url":"https://github.com/gittjar/BiisiGeneraattori", "update":"päivitetty 7.2.2023", "detail": "Konsoliohjelma C#, jossa integroituna CSV lukija, lisää, poista ja etsi biisejä, myös satunnaisia kappaleita. Ohjelma näyttää kappaleet ja linkin YouTubeen. Ohjelmalla on myös mahdollisuus lähettää sähköposteja.", "status":"valmis"},
+    {"id": 2006, "name": "C# Perusteet (MOOC)", "url":"../assets/images/MOOC-93percent.jpg","update":"päivitetty 17.2.2023","detail":"Verkkokurssi C# perusteet yht. 6 osiota, suoritettu 93% -> arvosteluna saatu kiitettävä.", "status":"valmis"},
   ]
+
+  sql = [{}]
 
 }
