fc6dc43aa0626fb05ad100eaed71510ce37bfba7
gittjar <jarno.keranen@gmail.com>
2024-03-10 19:34:20 +0200

    small fixes to layout

--- src/app/etusivu/etusivu.component.ts
+++ src/app/etusivu/etusivu.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-etusivu',
   templateUrl: './etusivu.component.html',
   styleUrls: ['./etusivu.component.css']
 })
-export class EtusivuComponent implements OnInit {
+export class EtusivuComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    $('#carouselExampleCaptions').carousel({
+      interval: 5000,
+      ride: true
+    });
   }
 
 }
