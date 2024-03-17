6b6d06d070b2f18fa9d3c37f49150490edf67001
gittjar <jarno.keranen@gmail.com>
2024-03-17 16:04:15 +0200

    carousel repaired

--- src/app/etusivu/etusivu.component.ts
+++ src/app/etusivu/etusivu.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 declare var $: any;
 
 @Component({
   selector: 'app-etusivu',
   templateUrl: './etusivu.component.html',
-  styleUrls: ['./etusivu.component.css']
+  styleUrls: ['./etusivu.component.css'],
+  animations: [
+    trigger('fadeAnimation', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('500ms', style({ opacity: 1 }))
+      ]),
+      transition('* => void', [
+        animate('1500ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class EtusivuComponent implements OnInit, AfterViewInit {
-
   constructor() { }
 
   ngOnInit(): void {
@@ -20,5 +31,4 @@ export class EtusivuComponent implements OnInit, AfterViewInit {
       ride: true
     });
   }
-
 }
